754a7febc1bd7c3a1c6ecf2b08a258524c5cc66e
maranvaraiaz <maranvaraiaz@gmail.com>
2020-02-08 23:56:40 -0600

    Proyecto HelloWorld
    Agregando archivos del proyecto HelloWorld

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 2020-2 on 07/02/20.
+//  Copyright © 2020 Marco Antonio Alvarado Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
